e21b534330e2e3b903311eda13d7e541d070ee7b
MakotoBkmz <131294713+MakotoBkmz@users.noreply.github.com>
2023-05-04 20:54:14 +0200

    Update homework_08.py

--- homework_08.py
+++ homework_08.py
@@ -1,31 +1,35 @@
 login = input('Please enter your Login (letters only) >>> ')
 password = input('Please enter your Password (letters and numbers only)  >>> ')
 confirm_password = input('Please confirm your password (letters and numbers only) >>> ')
+
+login_success_template = (f'Welcome {login}, you have successfully logged in.')
+password_match_false = (f'{login} your passwords do not match, please try again.')
 login_requirements_met = login.isalpha()
 password_syntax_met = password.isalnum() and confirm_password.isalnum()
 password_requirements_met = (password == confirm_password)
 
 if password_syntax_met and login_requirements_met:
     if password_requirements_met:
-        print(f'Welcome {login}, you have successfully logged in.')
+        print(login_success_template)
         print('You have been logged out.')
         if password_syntax_met:
             if password_requirements_met:
-                l=input('Please re-enter your Login (letters and numbers only)  >>> ')
-                p=input('Please confirm your password (letters and numbers only) >>> ')
+                l = input('Please re-enter your Login (letters and numbers only)  >>> ')
+                p = input('Please confirm your password (letters and numbers only) >>> ')
                 if l == login and p == password:
-                    print(f'Welcome {login}, you have successfully logged in.')
+                    print(login_success_template)
                 else:
-                    print(f'{login} your passwords do not match, please try again.')
+                    print(password_match_false)
             else:
-                print(f'{login} your passwords do not match, please try again.')
+                print(password_match_false)
         else:
             print(f'Please enter proper login or password!')
     else:
-        print(f'{login} your passwords do not match, please try again.')
+        print(password_match_false)
 
 else:
     print(f'Please enter proper login or password!')
+
 
 
 
