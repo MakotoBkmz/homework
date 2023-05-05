d72aba149073231d6ba3330f450a001a79c1a82d
MakotoBkmz <131294713+MakotoBkmz@users.noreply.github.com>
2023-05-05 17:49:07 +0200

    Delete homework_06.py

--- homework_06.py
+++
@@ -1,8 +0,0 @@
-from decimal import Decimal
-import math
-
-radius = float(input('Input radius here >>> '))
-sphere_volume = (4/3)*math.pi*radius**3
-
-result = Decimal(str(sphere_volume)).quantize(Decimal('0.01'))
-print(result)
