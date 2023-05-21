cbe21ddfd6621a1be28bb5516a9179c0ab5aa013
MakotoBkmz <131294713+MakotoBkmz@users.noreply.github.com>
2023-05-21 07:56:59 +0200

    Update homework_13.py

--- homework_13.py
+++ homework_13.py
@@ -5,10 +5,10 @@ data = response.json()
 
 louisville_citizen = [user for user in data["users"] if user["address"]["city"] == "Louisville"]
 men_with_brown_hair = [user["age"] for user in data["users"] if user["gender"] == "male" and user["hair"]["color"] == "Brown"]
-average_age_brown_hair_men = sum(men_with_brown_hair) / len(men_with_brown_hair)
+average_age_men_with_brown_hair = sum(men_with_brown_hair) / len(men_with_brown_hair)
 
 print("Список мужчин с коричневыми волосами в городе Louisville:")
 for person in louisville_citizen:
     print(person["firstName"], person["lastName"])
 
-print("Средний возраст мужчин с каштановыми волосами:", average_age_brown_hair_men)
+print("Средний возраст мужчин с каштановыми волосами:", average_age_men_with_brown_hair)
